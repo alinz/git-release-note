cd8e86de7bb9ae32b51c2b9504c6ba4ff1681c06
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-06-02 17:05:34 -0400

    first release

---
+++ README.md
@@ -0,0 +1,38 @@
+## Git Release Note
+
+if you are using github and git flow for your project, you can use this code to generate the release note.
+
+### Installation
+
+npm install git-release-note -g
+
+### Usage
+
+inside your repo just run the following command and the tool, generate the release note
+
+```bash
+gen-release-note
+```
+
+### Suggestion
+
+for the tool to work well, you have to follow git flow.
+
+for any feature and bug fixes use
+
+```bash
+git flow feature start #<issue number>
+
+...implement or fix the code
+
+git flow feature finish #<issue number>
+```
+
+once you close bunch of issues make a release
+
+```bash
+git flow release start v1.0.0
+git flow release finish v1.0.0
+```
+
+Once that one is done, just run the git-release-note to generate your release note from commits that git flow produces.
---
+++ index.js
@@ -0,0 +1,73 @@
+
+'use strict';
+
+var Rx = require('rx');
+var spawn = require('child_process').spawn;
+
+function splitBuffer(buffer, split, cb) {
+  buffer.toString('utf8').split(split).forEach(function (item) {
+    cb(item);
+  });
+}
+
+function rxSpwan(command, args, options) {
+  options = options || { cwd: '.' };
+
+  return Rx.Observable.create(function (observer) {
+    var spawnChild = spawn(command, args, options);
+
+    spawnChild.stdout.on('data', function (data) {
+      splitBuffer(data, '\n', function (chunk) {
+        observer.onNext({ type: 'data', data: chunk });
+      });
+    });
+
+    spawnChild.stderr.on('data', function (data) {
+      splitBuffer(data, '\n', function (chunk) {
+        observer.onNext({ type: 'error', data: chunk });
+      });
+    });
+
+    spawnChild.on("close", function (errorCode) {
+      observer.onCompleted();
+    });
+  });
+}
+
+function genReleateNote() {
+  var source = rxSpwan('git', ['log', '--pretty=oneline']);
+
+  source.filter(function (item) {
+    return item.type === 'data' && item.data && item.data !== '';
+  }).map(function (line) {
+    return line.data.match(/([0-9abcdef]{40}) (.+)/);
+  }).filter(function (group) {
+    return group.length == 3;
+  }).map(function (group) {
+    return {
+      hash: group[1],
+      message: group[2]
+    };
+  }).map(function (commit) {
+    commit.info = commit.message.match(/Merge branch '(.+)\/(.+)'/);
+    return commit;
+  }).filter(function (commit) {
+    return commit.info && commit.info.length === 3;
+  }).map(function (commit) {
+    commit.info = {
+      issue: commit.info[1],
+      number: commit.info[2]
+    };
+    return commit;
+  }).forEach(function (commit) {
+    var info = commit.info;
+    if (info.issue === 'release') {
+      console.log('');
+      console.log(info.number);
+    } else {
+      console.log('    Fixed issue ' + info.number);
+    }
+  });
+}
+
+genReleateNote();
